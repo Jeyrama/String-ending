885049f80db7585daa5b6bc8c818d72218b4ed11
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-09-17 17:21:27 -0700

    Update String-ending.js

--- String-ending.js
+++ String-ending.js
@@ -7,3 +7,11 @@ Examples:
 
 
 // Solution
+
+function solution(str, ending){
+  if (typeof(str) != "string" || typeof(ending) != "string")
+    throw "wrong type";
+  if (ending.length>str.length)
+    return false;
+  return str.substr(str.length-ending.length, ending.length) == ending;
+}
